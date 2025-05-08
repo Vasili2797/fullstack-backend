6d206db825951ddaa522cfc39ac535650e4d12af
Vasili2797 <nashvilliv1@gmail.com>
2025-05-08 02:27:27 -0400

    Added backedn program to get users or specific user from database

---
+++ src/main/java/com/codewitharjun/fullstack_backend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.codewitharjun.fullstack_backend.controller;
+
+import com.codewitharjun.fullstack_backend.exception.UserNotFoundException;
+import com.codewitharjun.fullstack_backend.model.User;
+import com.codewitharjun.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+}
